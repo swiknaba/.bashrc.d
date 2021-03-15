d3601542003d91c612dfc767d2db6b980b5ec031
swiknaba <swiknaba@users.noreply.github.com>
2021-03-15 17:53:10 +0100

    write launch agent to set kraken pump level

---
+++ launchd_scripts/kraken.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#
+# :warning: do not edit here, but edit the original file!
+# this is a copy from src/machines/hackintosh.bashrc
+#
+
+if command -v liquidctl > /dev/null
+then
+  CURRENT_PUMP_DUTY="$(liquidctl status | grep duty | awk '{print $4}')"
+  TARGET_PUMP_SPEED=${1:-70} # first argument or 70
+  if [[ $CURRENT_PUMP_DUTY != $TARGET_PUMP_SPEED ]]
+  then
+    eval "(liquidctl set pump speed $TARGET_PUMP_SPEED)"
+  fi
+  liquidctl status
+else
+  echo "liquidctl is not installed. Run 'brew install liquidctl' to install it."
+fi
